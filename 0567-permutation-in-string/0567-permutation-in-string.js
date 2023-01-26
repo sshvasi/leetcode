f44ce28b178ee29c4a08358e4dcaa844c0a25c2f
Alexander Vasiliev <asvasi25@gmail.com>
2023-01-26 14:31:16 +0400

    Time: 91 ms (76.88%), Space: 44.8 MB (63.33%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -2,11 +2,10 @@ const checkInclusion = (s1, s2) => {
   const needs = new Map();
   const window = new Map();
 
-  let match = 0;
-  let contains = false;
-
   let left = 0;
   let right = 0;
+  let match = 0;
+  let contains = false;
 
   for (const char of s1) {
     const count = needs.get(char) ?? 0;
@@ -14,30 +13,25 @@ const checkInclusion = (s1, s2) => {
   }
 
   while (right < s2.length) {
-    const rightChar = s2[right];
-    if (needs.has(rightChar)) {
-      const count = window.get(rightChar) ?? 0;
-      window.set(rightChar, count + 1);
+    if (needs.has(s2[right])) {
+      const count = window.get(s2[right]) ?? 0;
+      window.set(s2[right], count + 1);
 
-      if (window.get(rightChar) === needs.get(rightChar)) {
+      if (window.get(s2[right]) === needs.get(s2[right])) {
         match++;
       }
     }
 
     while (match === needs.size) {
-      const windowLen = right - left + 1;
-      if (windowLen === s1.length) {
+      if (right - left + 1 === s1.length) {
         contains = true;
       }
 
-      const leftChar = s2[left];
-      if (needs.has(leftChar)) {
-        const count = window.get(leftChar);
-        window.set(leftChar, count - 1);
-
-        if (window.get(leftChar) < needs.get(leftChar)) {
-          match--;
-        }
+      const count = window.get(s2[left]);
+      window.set(s2[left], count - 1);
+      
+      if (window.get(s2[left]) < needs.get(s2[left])) {
+        match--;
       }
 
       left++;
