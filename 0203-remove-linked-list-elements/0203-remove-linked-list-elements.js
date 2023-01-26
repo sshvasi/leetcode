0b2cb2c1e5dc5e79d173e1a12c9f07571b82084d
Alexander Vasiliev <asvasi25@gmail.com>
2023-01-26 10:12:20 +0400

    Time: 141 ms (11.07%), Space: 46.9 MB (31.63%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.js
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.js
@@ -5,14 +5,20 @@ const removeElements = (head, val) => {
   let current = head;
 
   while (current !== null) {
+    // Если значение «плохое», меняем ссылку предыдущего узла,
+    // и оставляем указатель на нём.
     if (current.val === val) {
       previous.next = current.next;
+    // Если значение «хорошее», перемещаем указатель на узел с «хорошим» значением.  
     } else {
       previous = current;
     }
 
+    // В любом случае двигаем текущий указатель.
     current = current.next;
   }
 
+  // Возвращаем следующий узел после заглушки,
+  // так как она не входит в реальный список.
   return sentinel.next;
 };
