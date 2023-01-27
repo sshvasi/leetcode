70204458984e096c8824f4c777d857d26a398a70
Alexander Vasiliev <asvasi25@gmail.com>
2023-01-27 11:19:07 +0400

    Time: 64 ms (74.93%), Space: 42.2 MB (52.24%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
@@ -1,20 +1,36 @@
-const inorderTraversal = (root) => {
+const inorderTraversal = function (root) {
   const values = [];
-  const stack = [];
 
-  let current = root;
-
-  while (current !== null || stack.length !== 0) {
-    while (current !== null) {
-      stack.push(current);
-      current = current.left;
+  const dfs = (node) => {
+    if (node !== null) {
+      dfs(node.left);
+      values.push(node.val);
+      dfs(node.right);
     }
+  };
 
-    const last = stack.pop();
-    values.push(last.val);
-
-    current = last.right;
-  }
+  dfs(root);
 
   return values;
 };
+
+// const inorderTraversal = (root) => {
+//   const values = [];
+//   const stack = [];
+
+//   let current = root;
+
+//   while (current !== null || stack.length !== 0) {
+//     while (current !== null) {
+//       stack.push(current);
+//       current = current.left;
+//     }
+
+//     const last = stack.pop();
+//     values.push(last.val);
+
+//     current = last.right;
+//   }
+
+//   return values;
+// };
