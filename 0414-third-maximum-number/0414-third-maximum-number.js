a6ae685be3d7f6905d7ebb5c259b8d16c8171860
Alexander Vasiliev <asvasi25@gmail.com>
2023-01-28 10:18:23 +0400

    Time: 65 ms (86.46%), Space: 43.7 MB (67.42%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.js
@@ -0,0 +1,32 @@
+const thirdMax = (nums) => {
+  let max1 = -Infinity;
+  let max2 = -Infinity;
+  let max3 = -Infinity;
+
+  for (const num of nums) {
+    if (
+      num === max1 ||
+      num === max2 ||
+      num === max3
+    ) {
+      continue;
+    }
+    
+    if (num > max1) {
+      max3 = max2;
+      max2 = max1;
+      max1 = num;
+    } else if (num > max2) {
+      max3 = max2;
+      max2 = num;
+    } else if (num > max3) {
+      max3 = num;
+    }
+  }
+  
+  if (max3 === -Infinity) {
+    return max1;
+  }
+
+  return max3;
+};
