18ab2f9d883e447d47e5875bd2bff8c36799cf36
Alexander Vasiliev <asvasi25@gmail.com>
2023-01-29 12:22:32 +0400

    Time: 75 ms (84.64%), Space: 44.4 MB (88.59%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,14 @@
+const removeDuplicates = (nums) => {
+  let left = 1;
+  let right = 1;
+
+  while (right < nums.length) {
+    if (nums[right] !== nums[right - 1]) {
+      nums[left] = nums[right];
+      left++;
+    }
+    right++;
+  }
+
+  return left;
+};
