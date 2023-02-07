6df356aad1c69a972489ee7a9bca443da64432c7
Alexander Vasiliev <asvasi25@gmail.com>
2023-02-07 06:12:44 +0400

    Time: 76 ms (32.58%), Space: 41.6 MB (94.00%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -1,19 +1,17 @@
 const merge = (nums1, m, nums2, n) => {
-  let i = m - 1;
-  let j = n - 1;
-  let k = n + m - 1;
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
 
-  while (j >= 0) {
-    const isFirstGreater = nums1[i] > nums2[j];
-    // nums1[k--] = isFirstGreater ? nums1[i--] : nums2[j--];
-    if (i >= 0 && isFirstGreater) {
-      nums1[k] = nums1[i];
-      k--;
-      i--;
-    } else {
-      nums1[k] = nums2[j];
-      k--;
-      j--;
+    while (i >= 0 && j >= 0) {
+        nums1[k--] = nums1[i] > nums2[j]
+            ? nums1[i--]
+            : nums2[j--]
     }
-  }
+
+    if (j >= 0) {
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
 };
