0a4feb04837d47bb0c17243a7cf10c0a750032d5
Alexander Vasiliev <asvasi25@gmail.com>
2023-02-10 23:07:57 +0400

    Time: 80 ms (62.54%), Space: 44.2 MB (84.03%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -1,7 +1,7 @@
 const isPalindrome = (s) =>{
     s = s.toUpperCase().replace(/[^a-z0-9]/gi, '')
     
-    for (let i = 0; i < s.length; i++) {
+    for (let i = 0; i < s.length / 2; i++) {
         const j = s.length - i - 1
         
         if (s[i] !== s[j]) {
