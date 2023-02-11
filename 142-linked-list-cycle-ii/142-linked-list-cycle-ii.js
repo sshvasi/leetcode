9cbecdd43504ced0f0431dc50adc93adbc2d62e0
Alexander Vasiliev <asvasi25@gmail.com>
2023-02-11 21:03:45 +0400

    Time: 76 ms (85.46%), Space: 44.6 MB (72.28%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.js
@@ -0,0 +1,21 @@
+const detectCycle = function(head) {
+    let slow = head
+    let fast = head
+    let entry = head
+    
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next
+        fast = fast.next.next 
+        
+        if (slow === fast) {
+            while (entry !== slow) {
+                entry = entry.next
+                slow = slow.next
+            }
+            
+            return entry
+        }
+    }
+        
+    return null
+};
