6d52ea8e40b0b7f26bd98c662f383c3ffd81d011
Alexander Vasiliev <asvasi25@gmail.com>
2023-03-20 19:45:13 +0400

    Time: 56 ms, Memory: 42.1 MB - LeetHub

---
+++ merge-sorted-array/merge-sorted-array.js
@@ -0,0 +1,17 @@
+const merge = (nums1, m, nums2, n) => {
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
+
+    while (i >= 0 && j >= 0) {
+        nums1[k--] = nums1[i] > nums2[j]
+            ? nums1[i--]
+            : nums2[j--]
+    }
+
+    if (j >= 0) {
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+};
