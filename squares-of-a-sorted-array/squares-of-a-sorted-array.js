4ed976fb5e8bec11fd4d0026d07ba6f76fd933ea
Alexander Vasiliev <asvasi25@gmail.com>
2023-03-20 19:21:33 +0400

    Time: 95 ms, Memory: 49 MB - LeetHub

---
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -0,0 +1,23 @@
+const sortedSquares = (nums) => {
+  const n = nums.length;
+  
+  const squares = Array(n);
+  
+  let left = 0;
+  let right = n - 1;
+
+  for (let high = n - 1; high >= 0; high--) {
+    const leftSquare = nums[left] ** 2;
+    const rightSquare = nums[right] ** 2;
+
+    if (leftSquare > rightSquare) {
+      squares[high] = leftSquare;
+      left++;
+    } else {
+      squares[high] = rightSquare;
+      right--;
+    }
+  }
+
+  return squares;
+}
