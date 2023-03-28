745e35469fbb17a904ac2b92e6338c9ccfc1092a
Alexander Vasiliev <asvasi25@gmail.com>
2023-03-28 23:38:27 +0300

    Time: 101 ms, Memory: 55.3 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.js
@@ -0,0 +1,12 @@
+const containsDuplicate = (nums) => {
+  const set = new Set();
+
+  for (const num of nums) {
+    if (set.has(num)) {
+      return true
+    }
+    set.add(num)
+  }
+  
+  return false
+};
