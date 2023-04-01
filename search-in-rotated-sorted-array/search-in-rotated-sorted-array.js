63450e15bbac051081ce5b4adb705d82762680f8
Alexander Vasiliev <asvasi25@gmail.com>
2023-04-01 22:32:41 +0300

    Time: 57 ms, Memory: 41.8 MB - LeetHub

---
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -0,0 +1,28 @@
+const search = (nums, target) => {
+  let left = 0
+  let right = nums.length - 1
+
+  while (left <= right) {
+    const mid = Math.floor(left + (right - left) / 2)
+
+    if (nums[mid] === target) {
+      return mid
+    }
+
+    if (nums[mid] >= nums[left]) {
+      if (target >= nums[left] && target < nums[mid]) {
+        right = mid - 1
+      } else {
+        left = mid + 1
+      }
+    } else {
+      if (target <= nums[right] && target > nums[mid]) {
+        left = mid + 1
+      } else {
+        right = mid - 1
+      }
+    }
+  }
+
+  return -1
+}
