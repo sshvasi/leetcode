098b2c4cdbae6bbeedf157ebbfa16090e03bece2
Alexander Vasiliev <asvasi25@gmail.com>
2023-04-08 01:44:48 +0300

    Time: 83 ms, Memory: 49.9 MB - LeetHub

---
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -0,0 +1,11 @@
+const getIntersectionNode = (headA, headB) => {
+  let currA = headA
+  let currB = headB
+
+  while (currA !== currB) {
+    currA = currA ? currA.next : headB
+    currB = currB ? currB.next : headA
+  }
+
+  return currA
+}
